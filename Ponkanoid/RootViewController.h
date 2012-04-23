59570662f8aa268c2980907c44abcb1b793b9043
Kevin <kevin@admins-MacBook-Pro.local>
2012-04-23 23:13:17 +0200

    Initial Commit

---
+++ Ponkanoid/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Ponkanoid
+//
+//  Created by Kevin Bruin on 4/23/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Ponkanoid/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ponkanoid
+//
+//  Created by Kevin Bruin on 4/23/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
