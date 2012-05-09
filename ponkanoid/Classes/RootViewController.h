6538e16c814e786d687f97c6ae1e63531cf50480
Paul Brinkkemper <paulbrinkkemper@Paul-Brinkkempers-MacBook.local>
2012-05-09 21:18:45 +0200

    cocos2d project started

---
+++ ponkanoid/Classes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  ponkanoid
+//
+//  Created by Paul Brinkkemper on 4/24/12.
+//  Copyright ITu 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ ponkanoid/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ponkanoid
+//
+//  Created by Paul Brinkkemper on 4/24/12.
+//  Copyright ITu 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"ponkanoidAppDelegate");
+	[pool release];
+	return retVal;
+}
